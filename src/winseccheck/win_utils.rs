d8b7033c67a83201b2afb5b15f56397af18ce9ec
Anthony <ayann1@unh.newhaven.edu>
2025-03-22 18:08:58 -0400

    commit

--- src/winseccheck/win_utils.rs
+++ src/winseccheck/win_utils.rs
@@ -2,6 +2,8 @@
 
 //create function to check windows defender settings
 
+//check if windows defender is enabled, and settings are correct
+//if settings not correct, create option to set the settings all together
 fn check_windows_defender() -> bool {
-    
+
 }
