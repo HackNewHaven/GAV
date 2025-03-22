94d11e6fd21cf2db7a0bb090e35c924a95c9aa39
Anthony <ayann1@unh.newhaven.edu>
2025-03-22 16:53:42 -0400

    add db connection functionality

--- src/pwmanager/functionality/pw_utils.rs
+++ src/pwmanager/functionality/pw_utils.rs
@@ -4,6 +4,17 @@ use std::env;
 use std::error::Error;
 use tokio;
 
+#[tokio::main]
+async fn connect_to_db() -> Result<(), Box<dyn error>> {
+    let client_uri =
+        env::car("DB_URI").expect("Please setup MongoDB_URI enviorment variable!");
+
+        let options =
+            ClientOptions::parse_with_resolver_config(client_uri, ResolverConfig::cloudflare()).await?;
+            let client = Client::with::options(options)?;
+        }
+
+    ok(())
 // Generates a random password
 pub fn generate_secure()-> String {
     let mut rng = rand::rng();
