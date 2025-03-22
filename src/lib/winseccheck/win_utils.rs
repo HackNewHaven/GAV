5bccb1608c673fede2d776d2a766d18bfdebc090
Anthony <ayann1@unh.newhaven.edu>
2025-03-22 19:14:27 -0400

    Ilovewindows

--- src/lib/winseccheck/win_utils.rs
+++ src/lib/winseccheck/win_utils.rs
@@ -6,32 +6,23 @@ use windows_registry::*;
 
 //check if windows defender is enabled, and settings are correct
 //if settings not correct, create option to set the settings all together
-pub fn check_windows_defender() {
+pub fn check_windows_defender() -> Result<()>{
     // Open registry for Windows Defender
     let key = LOCAL_MACHINE.create(r"SOFTWARE\Policies\Microsoft\Windows Defender")?;
 
     //check if DisableAntiSpyware is set to 0 (enabled)
-    let defender_enabled = key.get_value("DisableAntiSpyware").unwrap_or(0) == 0;
-    //check if DisableRealtimeMonitoring is ebabled
-    let realtime_enabled = key.get_value("DisableRealtimeMonitoring").unwrap_or(0) == 0;
-
-    //if disabled notify user and prompt to enable
-}
-
-pub fn check_windows_defender_policies() {
-    // Open registry for Windows Defender policies
-    let key = LOCAL_MACHINE.create(r"SOFTWARE\Policies\Microsoft\Windows Defender")?;
-    //check if DisableAntiSpyware is set to 0 (enabled)
-    let defender_enabled = LOCAL_MACHINE.create("DisableAntiSpyware").unwrap_or(0) == 0;
-    //check if DisableRealtimeMonitoring is ebabled
-    let realtime_enabled = key.get_value("DisableRealtimeMonitoring").unwrap_or(0) == 0;
+    let defender_enabled = key.get_u32("DisableAntiSpyware")?;
+    //check if DisableRealtimeMonitoring is ebabledc
+    //let realtime_enabled = key.get_value("DisableRealtimeMonitoring").unwrap_or(Value::Dword(0)) != Value::(0);
 
     //if disabled notify user and prompt to enable
 
+    ok(())
 }
+
 
 pub fn check_currentctrlset(){
     let key = LOCAL_MACHINE.create(r"HKLM\SYSTEM\CurrentControlSet\Services\WinDefend")?;
-    let win_defendenabled = key.get_value("Start").unwrap_or(4) == 4;
+    let win_defendenabled = key.get_value("Start").unwrap_or(Value::Dword(4)) != Value::Dword(4);
     
 }
