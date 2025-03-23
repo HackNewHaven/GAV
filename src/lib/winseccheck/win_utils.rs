0e2d0c40da41fce20b9114c611f57a2d9a720ed4
Anthony <ayann1@unh.newhaven.edu>
2025-03-22 21:09:14 -0400

    windows add

--- src/lib/winseccheck/win_utils.rs
+++ src/lib/winseccheck/win_utils.rs
@@ -8,12 +8,57 @@ use windows_registry::*;
 //if settings not correct, create option to set the settings all together
 pub fn check_windows_defender() -> bool {
     // Open registry for Windows Defender
-    let val = LOCAL_MACHINE
+    let DisableAntiSpy = LOCAL_MACHINE
     .open(r#"SOFTWARE\Microsoft\Windows Defender"#)
     .expect("Failed to get registry key")
     .get_u32("DisableAntiSpyware")
     .expect("Failed to get registry value");
-val != 0
+//DisableAntiSpy != 0;
+
+let DisableAV = LOCAL_MACHINE
+    .open(r#"SOFTWARE\Microsoft\Windows Defender"#)
+    .expect("Failed to get registry key")
+    .get_u32("DisableAntiVirus")
+    .expect("Failed to get registry value");
+//DisableAV != 0;
+let tamperProtection = LOCAL_MACHINE
+    .open(r#"SOFTWARE\Microsoft\Windows Defender\Features"#)
+    .expect("Failed to get registry key")
+    .get_u32("DisableTamperProtection")
+    .expect("Failed to get registry value");
+
+
+    //check if real time monitoring is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if cloud protection is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if automatic sample submission is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if exploit protection is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if controlled folder access is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if firewall is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if network protection is enabled
+    //if not enabled notify user and prompt to enable
+
+    //check if ransomware protection is enabled
+    //if not enabled notify user and prompt to enable
+
+
+    println!("Windows Defender is disabled: {}", DisableAntiSpy);
+    println!("Anti-Virus is disabled {}", DisableAV);
+    println!("Tamper Protection is disabled {}", tamperProtection);
+    //if any of the above are disabled return false, else return true
+    
+    true//ok(())
 }
 
     //check if DisableAntiSpyware is set to 0 (enabled)
