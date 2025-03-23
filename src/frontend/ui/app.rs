d403d97e1aae305c16436238074404b84d20928b
Anthony <ayann1@unh.newhaven.edu>
2025-03-23 07:26:22 -0400

    button

--- src/frontend/ui/app.rs
+++ src/frontend/ui/app.rs
@@ -67,13 +67,12 @@ impl eframe::App for FrontendApp {
             ui.heading("GAV User Interface");
             ui.label("Check windows security settings");
             if ui.button("Check").clicked() {
-                //call function for checking windows security settings
+                //call function for check_windows_defender();
                 println!("Button clicked!");
             }
             
             ui.label("Enable Windows Defender");
             if ui.button("Enable").clicked() {
-                //call function for enabling windows defender
                 println!("Button clicked!");
             }
             /*
